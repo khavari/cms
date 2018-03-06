77756b4c415ca94dad66f6bf6da64af8669cc4a9
Mohammad Khavari <khavari.mvc@gmail.com>
2018-03-07 01:20:14 +0330

    Fix for v3

--- resources/views/web/widgets/bootstrap-navbar-sticky.blade.php
+++ resources/views/web/widgets/bootstrap-navbar-sticky.blade.php
@@ -118,7 +118,7 @@
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title"  for="search"  id="search-modallLabel">@lang('web.search')</h5>
+                                                <label class="modal-title"  for="search"  id="search-modallLabel">@lang('web.search')</label>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
