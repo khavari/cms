e6a19af02d220ba97ae157dbd4d533f90c9f25ff
MostafaKhavari <web.khavari@gmail.com>
2018-03-15 22:42:48 +0330

    edit page

--- resources/views/web/widgets/page.blade.php
+++ resources/views/web/widgets/page.blade.php
@@ -28,11 +28,129 @@
                         <div class="header border_vibrant">
                             <h2 class="title pr-0 pl-0">{{ $content->title }}</h2>
                         </div>
-                    <div class="body">
+                    <div class="editor">
+                        <table class="table table-striped table-hover table-bordered">
+                            <thead class="bg_vibrant text_bg_vibrant ">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                            </thead>
+                            <tbody class="">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
                         {!! $content->body !!}
                     </div>
+
                 </div>
             </div>
                   </div>
+        <div class="gallery mb-3">
+            <div class="row">
+                    <h2 class="title border_vibrant">@lang('web.gallery')</h2>
+            </div>
+        <div class="row">
+            @for($i=0;$i<8;$i++)
+            <div class="col-3 mb-4">
+                <img src="{{ asset($content->image) }}" class="img-fluid animated fadeIn"
+                     alt="{{ $content->title }}">
+               </div>
+            @endfor
+        </div>
+        </div>
+        <div class="video mb-3">
+            <div class="row">
+                <h2 class="title border_vibrant">@lang('web.video')</h2>
+            </div>
+            <div class="row">
+                @for($i=0;$i<8;$i++)
+                    <div class="col-12 col-xs-6 col-sm-4 col-md-3 col- mb-4">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                        </div>
+
+                    </div>
+                @endfor
+            </div>
+        </div>
+        <div class="download mb-3">
+            <div class="row">
+                <h2 class="title border_vibrant">@lang('web.download')</h2>
+            </div>
+            <div class="row">
+                @for($i=0;$i<8;$i++)
+                    <div class="col-6 col-md-3  mb-4">
+                        <a href="#">
+                            <div class="image">
+                        {{--<img src="{{ asset($content->image) }}" class="img-fluid animated fadeIn"--}}
+                             {{--alt="{{ $content->title }}">--}}
+                            </div>
+                            <div class="content">
+                                <div class="title-inner">@lang('lorem.3')</div>
+                                <div class="body">@lang('lorem.6')</div>
+                            </div>
+                        </a>
+                    </div>
+                @endfor
+            </div>
+        </div>
+        <div class="comment mb-3">
+            <div class="row">
+                <h2 class="title border_vibrant">@lang('web.comment')</h2>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn-modal border_vibrant" data-toggle="modal" data-target="#comment-modal">
+                    @lang('web.write_comment')
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="comment-modalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="comment-modalLabel">@lang('web.send_comment')</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="" method="get">
+                                    <textarea name="" id="" class="textarea" placeholder="@lang('web.text_modal_comment')"></textarea>
+                                    <button type="button" class="btn submit">@lang('web.submit_comment')</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary " data-dismiss="modal">@lang('web.close')</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-12 mb-4">
+
+                    </div>
+
+            </div>
+        </div>
     </div>
 </section>
