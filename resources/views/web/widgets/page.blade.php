d00eaa225657912828a3b32cdb6c2261c434ee6e
MostafaKhavari <web.khavari@gmail.com>
2018-03-16 01:56:08 +0330

    edit comments

--- resources/views/web/widgets/page.blade.php
+++ resources/views/web/widgets/page.blade.php
@@ -115,14 +115,14 @@
             </div>
         </div>
         <div class="comment mb-3">
+            <div class="comment-header">
             <div class="row">
-                <h2 class="title border_vibrant">@lang('web.comment')</h2>
-                <!-- Button trigger modal -->
-                <button type="button" class="btn-modal border_vibrant" data-toggle="modal" data-target="#comment-modal">
+                <h2 class="title border_vibrant">@lang('web.comments')</h2>
+                <!-- Button write_comment modal -->
+                <button type="button" class="btn-modal border_vibrant text_vibrant" data-toggle="modal" data-target="#comment-modal">
                     @lang('web.write_comment')
                 </button>
-
-                <!-- Modal -->
+                <!--write_comment Modal -->
                 <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="comment-modalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
@@ -145,11 +145,120 @@
                     </div>
                 </div>
             </div>
+            </div>
+            <div class="comment-body mb-3">
+            <div class="first-level">
+            <div class="head">
             <div class="row">
-                    <div class="col-12 mb-4">
-
+                <div class="col-6 author-area">
+                    <span class="author">mostafa</span> &nbsp; | &nbsp; <span class="date">One day ago</span>
+                </div>
+                    <div class="col-6 btn-area">
+                        <!-- Button answer_comment modal -->
+                        <button type="button" class="btn-modal border_vibrant text_vibrant" data-toggle="modal" data-target="#comment-modal">
+                            @lang('web.answer_comment')
+                        </button>
+                        <!-- answer_comment Modal -->
+                        <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="comment-modalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="comment-modalLabel">@lang('web.send_comment')</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="" method="get">
+                                            <textarea name="" id="" class="textarea" placeholder="@lang('web.text_modal_comment')"></textarea>
+                                            <button type="button" class="btn submit">@lang('web.submit_comment')</button>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary " data-dismiss="modal">@lang('web.close')</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+            </div>
+            </div>
+            <div class="content">
+                @lang('lorem.10')
+            </div>
+            </div>
+            <div class="second-level">
+                <div class="head">
+                    <div class="row">
+                        <div class="col-12 author-area">
+                            <span class="author">Ali</span> &nbsp; | &nbsp; <span class="date"> Today</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    @lang('lorem.15')
+                    @lang('lorem.15')
+                    @lang('lorem.15')
+                </div>
+            </div>
+            </div>
 
+            <div class="comment-body mb-3">
+                <div class="first-level">
+                    <div class="head">
+                        <div class="row">
+                            <div class="col-6 author-area">
+                                <span class="author">mostafa</span> &nbsp; | &nbsp; <span class="date">One day ago</span>
+                            </div>
+                            <div class="col-6 btn-area">
+                                <!-- Button answer_comment modal -->
+                                <button type="button" class="btn-modal border_vibrant text_vibrant" data-toggle="modal" data-target="#comment-modal">
+                                    @lang('web.answer_comment')
+                                </button>
+                                <!-- answer_comment Modal -->
+                                <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="comment-modalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="comment-modalLabel">@lang('web.send_comment')</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="" method="get">
+                                                    <textarea name="" id="" class="textarea" placeholder="@lang('web.text_modal_comment')"></textarea>
+                                                    <button type="button" class="btn submit">@lang('web.submit_comment')</button>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary " data-dismiss="modal">@lang('web.close')</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        @lang('lorem.10')
+                    </div>
+                </div>
+                <div class="second-level">
+                    <div class="head">
+                        <div class="row">
+                            <div class="col-12 author-area">
+                                <span class="author">Ali</span> &nbsp; | &nbsp; <span class="date"> Today</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        @lang('lorem.15')
+                        @lang('lorem.15')
+                        @lang('lorem.15')
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
