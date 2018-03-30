331505500f28aaa96c934baa06533379776ea7e1
Mohammad Khavari <khavari.mvc@gmail.com>
2018-03-30 23:28:20 +0430

    fix for unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testBasicTest_()
     {
         $response = $this->get('/');
 
