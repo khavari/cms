4affa7d48a264a8a70c6d70fad2834e0660d3e89
Mohammad Khavari <khavari.mvc@gmail.com>
2018-04-01 15:10:25 +0430

    work on comments

--- resources/views/web/widgets/partial-comments.blade.php
+++ resources/views/web/widgets/partial-comments.blade.php
@@ -2,9 +2,15 @@
     <div class="comment-header">
         <div class="row align-items-center">
             <h2 class="title border_vibrant">@lang('web.comments')</h2>
+            @if(auth()->check())
             <button type="button" class="badge badge-primary" data-toggle="modal" data-target="#comment-modal">
                 @lang('web.write_comment')
             </button>
+            @else
+                <button type="button" class="badge badge-primary" data-toggle="modal" data-target="#user-login-modal">
+                    @lang('web.login_for_comment')
+                </button>
+            @endif
             <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="comment-modalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog" role="document">
@@ -22,8 +28,7 @@
                                 <input type="hidden" name="commentable_type" value="App\Content">
                                 <textarea name="message" id="message" class="textarea"
                                           placeholder="@lang('web.text_modal_comment')"></textarea>
-                                <button type="submit"
-                                        class="btn btn-primary submit">@lang('web.submit_comment')</button>
+                                <button type="submit" class="btn btn-primary submit">@lang('web.submit_comment')</button>
 
                             </form>
                         </div>
@@ -39,16 +44,28 @@
             <div class="first-level p-1 p-md-3">
                 <div class="head">
                     <div class="row align-items-center">
-                        <div class="col-12 col-sm-6 mb-3 author-area">
+                        <div class="col-12 col-sm-6 mb-3 commenter">
                             <span class="author">{{ $comment->user->name }}</span>
-                            &nbsp; | &nbsp;
-                            <span class="date">{{ $comment->created_at->diffForHumans() }}</span>
+                            <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                            @if(locale('dir') === 'ltr')
+                                <span class="date">{{ $comment->created_at->diffForHumans() }}</span>
+                            @else
+                                <span class="date">{{ jdate($comment->created_at)->ago() }}</span>
+                            @endif
                         </div>
                         <div class="col-12 col-sm-6 mb-3 btn-area">
-                            <button type="button" class="badge badge-primary" data-toggle="modal"
-                                    data-target="#reply{{ $comment->getKey() }}">
-                                @lang('web.answer_comment')
-                            </button>
+
+                            @if(auth()->check())
+                                <button type="button" class="badge badge-primary" data-toggle="modal"
+                                        data-target="#reply{{ $comment->getKey() }}">
+                                    @lang('web.answer_comment')
+                                </button>
+                            @else
+                                <button type="button" class="badge badge-primary" data-toggle="modal" data-target="#user-login-modal">
+                                    @lang('web.login_for_comment')
+                                </button>
+                            @endif
+
 
                             <div class="modal fade" id="reply{{ $comment->getKey() }}" tabindex="-1" role="dialog"
                                  aria-labelledby="comment-modalLabel" aria-hidden="true">
@@ -89,10 +106,14 @@
                     <div class="second-level mb-3 p-1 p-md-3">
                         <div class="head">
                             <div class="row">
-                                <div class="col-12 author-area">
+                                <div class="col-12 commenter">
                                     <span class="author">{{ $comment->user->name }}</span>
-                                    &nbsp; | &nbsp;
-                                    <span class="date">{{ $comment->created_at->diffForHumans() }}</span>
+                                    <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                                    @if(locale('dir') === 'ltr')
+                                        <span class="date">{{ $comment->created_at->diffForHumans() }}</span>
+                                    @else
+                                        <span class="date">{{ jdate($comment->created_at)->ago() }}</span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -104,3 +125,6 @@
     @endforeach
 
 </section>
+
+
+@includeIf('web.widgets.user-login-modal')
---
+++ resources/views/web/widgets/user-login-modal.blade.php
@@ -0,0 +1,51 @@
+{{-- user-authenticate-rtl.scss user-authenticate-ltr.scss --}}
+<div class="modal fade" id="user-login-modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="{{ route('login') }}" method="post">
+                {{ csrf_field() }}
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">@lang('web.login')</h4>
+                </div>
+                <div class="modal-body">
+
+                    {{--------------- email ---------------}}
+                    <div class="form-group">
+                        <label for="email">@lang('web.email')</label>
+                        <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email"
+                               value="{{ old('email') }}" name="email" required autofocus>
+                        @if ($errors->has('email'))
+                            <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                        @endif
+                    </div>
+                    {{--------------- password ---------------}}
+                    <div class="form-group">
+                        <label for="password">@lang('web.password')</label>
+                        <input type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}"
+                               id="password" name="password" required>
+                        @if ($errors->has('password'))
+                            <div class="invalid-feedback">{{ $errors->first('password') }}</div>
+                        @endif
+                    </div>
+                    {{--------------- label ---------------}}
+                    <div class="custom-control custom-checkbox my-1 mr-sm-2">
+                        <input type="checkbox" class="custom-control-input" id="remember"
+                               name="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="custom-control-label label-checkbox" for="remember">@lang('web.remember_me')</label>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button class="btn form-btn btn-primary" type="submit">@lang('web.login')</button>
+                    @if(env('GOOGLE_CLIENT_ID'))
+                        <a href="{{ route('socialite',['driver'=>'google']) }}" class="btn  login-btn border_vibrant "
+                           type="submit"> <i class="fa fa-google-plus"></i> @lang('web.sign_in_with_google-plus')</a>
+                    @endif
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
