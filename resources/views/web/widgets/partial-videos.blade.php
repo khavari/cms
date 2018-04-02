2b0a663389853f8590cfbf1d2de49bb9abd6cf2e
MostafaKhavari <web.khavari@gmail.com>
2018-04-02 12:35:30 +0430

    edit page

--- resources/views/web/widgets/partial-videos.blade.php
+++ resources/views/web/widgets/partial-videos.blade.php
@@ -1,15 +1,12 @@
 <div class="partial-videos mb-3">
     <div class="row">
-        <h2 class="title border_vibrant">@lang('web.video')</h2>
+        <h2 class="title">@lang('web.video')</h2>
     </div>
     <div class="row">
         @for($i=0;$i<8;$i++)
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col- mb-4">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-                </div>
-
-            </div>
+            <div class="col-12">
+ <div id="15226514319831810"><script type="text/JavaScript" src="https://www.aparat.com/embed/fXRUM?data[rnddiv]=15226514319831810&data[responsive]=yes"></script></div>
+                      </div>
         @endfor
     </div>
 </div>
